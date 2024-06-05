995ed55b4abc9f11b9f7faedf87a11db220388ce
eggnocent <egiwiratamaaa@gmail.com>
2024-06-05 08:41:20 +0700

    05/06/2024

---
+++ app/Models/Barang.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Kategori;
+
+class Barang extends Model
+{
+    use HasFactory;
+    protected $table = 'barang';
+
+    protected $fillable = ['merk','seri','spesifikasi','stok','kategori_id','foto'];
+
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class, 'kategori_id');
+    }
+    public function barangmasuk()
+    {
+        return $this->hasMany(BarangMasuk::class);
+    }
+    public function barangkeluar()
+    {
+        return $this->hasMany(BarangKeluar::class);
+    }
+
+}
